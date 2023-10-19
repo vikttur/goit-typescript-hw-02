680d260a611254e9f9d9861803036b7cd1a3e557
Viktor Biruk <vikttur.job@gmail.com>
2023-10-19 13:40:50 +0300

    performs task #3

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,13 @@
-let some:unknown;
+// -var1-
+let some: unknown;
 some = 'Text';
 let str: string;
-str = some;
+if (typeof some === 'string') str = some;
+
+// -var2-
+// let some: any;
+// some = 'Text';
+// let str: string;
+// str = some;
 
 export {};
