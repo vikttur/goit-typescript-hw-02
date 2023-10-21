4fd149699613fc915b3220934e8345e2c8d6f67f
Viktor Biruk <vikttur.job@gmail.com>
2023-10-21 17:08:59 +0300

    performs task #9

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,18 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type Pages = {
+	title: string;
+	likes: number;
+	accounts: string[];
+	status: string;
+	details?: {
+		createAt: string | number | Date,
+    updateAt: string | number | Date,
+	};
+}
 
-const page1 = {
+const page1: Pages = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +23,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Pages = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
