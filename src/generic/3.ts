5a687ee3c5e94fb6fc23d3988e0a3d29b7e535eb
Viktor Biruk <vikttur.job@gmail.com>
2023-10-29 00:26:38 +0300

    performs task (generic) #3

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge<T, U>(objA: { T: any }, objB: { U: any }) {
   return Object.assign(objA, objB);
 }
 
